0e66b8e7175866e1c94bcbed2100f2629dc1d4ad
KhahoryITLA <20164247@itla.edu.do>
2019-06-08 20:07:59 -0400

    Commit

---
+++ Televiisor_prueba2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Televiisor_prueba2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Canal25()
+        {
+
+            return View();
+        }
+
+        public ActionResult Canal21()
+        {
+
+            return View();
+        }
+
+        public ActionResult Canal7()
+        {
+
+            return View();
+        }
+
+        public ActionResult Canal5()
+        {
+
+            return View();
+        }
+
+        public ActionResult Canal4()
+        {
+
+            return View();
+        }
+
+        public ActionResult Canal9()
+        {
+
+            return View();
+        }
+
+        public ActionResult Canal15()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Televiisor_prueba2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Televiisor_prueba2.MvcApplication" Language="C#" %>
